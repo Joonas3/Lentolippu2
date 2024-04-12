40015a72a3661f05a2a7d90938e5a56f0699280e
Joonas <raitajoo@uef.fi>
2024-04-12 21:51:28 +0300

    Commit

--- HarjoitustyoKoodit/main/java/lentoliput/harjoitustyo/lentoliput/LipunLisatiedot.java
+++ HarjoitustyoKoodit/main/java/lentoliput/harjoitustyo/lentoliput/LipunLisatiedot.java
@@ -1,5 +1,17 @@
 package lentoliput.harjoitustyo.lentoliput;
 abstract class olionTiedot{
+    /**
+     * Metodi toteutetaan Lentolippu-luokassa
+     * Asettaa matkustajan matkustusluokan, joka voi olla joko 1, tai 2
+     * Jos asetetaan muuta kuin 1 tai 2, asettaa arvoksi 2
+     * @param matkustusluokka matkustajan matkustusluokka
+     */
     abstract void setMatkustusluokka(int matkustusluokka);
+
+    /**
+     * Metodi toteutetaan Lentolippu-luokassa
+     * Palauttaa lennolle annetut lähtö -ja kohdekaupunkien välimatkan
+     * @return etaisyys
+     */
     abstract double etaisyysMaarittelija();
 }
--- HarjoitustyoKoodit/main/java/module-info.java
+++ HarjoitustyoKoodit/main/java/module-info.java
@@ -2,7 +2,6 @@ module lentoliput.harjoitustyo.lentoliput {
     requires javafx.controls;
     requires javafx.fxml;
 
-
     opens lentoliput.harjoitustyo.lentoliput to javafx.fxml;
     exports lentoliput.harjoitustyo.lentoliput;
 }
